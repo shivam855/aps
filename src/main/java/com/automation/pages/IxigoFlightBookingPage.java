aed5e2f4181e5f66f58398b7ebbc224af1dbe724
shivam855 <shivam906824@gmail.com>
2026-06-08 21:46:35 +0530

    Final week 3

--- src/main/java/com/automation/pages/IxigoFlightBookingPage.java
+++ src/main/java/com/automation/pages/IxigoFlightBookingPage.java
@@ -1,11 +1,18 @@
-package com.qa.pages;
+package com.automation.pages;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public final class IxigoFlightBookingPage {
+public class IxigoFlightBookingPage  extends BasePage {
 
-    private IxigoFlightBookingPage() {
+    public IxigoFlightBookingPage() {
     }
+    
+    private static final long LOGIN_SETTLE_WAIT_MS = 10000;
 
     public static final By LOGIN_SIGNUP_BUTTON = By.xpath("(//button[text()='Log in/Sign up'])[1]");
     public static final By MOBILE_NUMBER_INPUT = By.xpath("//input[@aria-label='Enter Mobile Number' or @placeholder='Enter Mobile Number']");
@@ -20,7 +27,7 @@ public final class IxigoFlightBookingPage {
     public static final By KOLKATA_OPTION = By.xpath("(//p[contains(normalize-space(),'Kolkata, West Bengal, India')])[1]");
 
     public static final By DEPARTURE_DATE = By.cssSelector("[data-testid='departureDate']");
-    public static final By JULY_10_2026_BUTTON = By.xpath("(//button/*[@aria-label=\"10 July 2026\"])[last()]");
+    public static final By JULY_10_2026_BUTTON = By.xpath("(//button/*[@aria-label=\"July 10, 2026\"])[last()]");
     public static final By PAX_DROPDOWN = By.cssSelector("[data-testid='pax']");
     public static final By PAX_COUNT_ONE = By.xpath("(//button[@data-testid='1'])[1]");
     public static final By DONE_BTN = By.xpath("//button[text()='Done']");
@@ -46,5 +53,136 @@ public final class IxigoFlightBookingPage {
     public static final By CARD_EXP_DATE = By.cssSelector("[data-testid='card-exp-date']");
     public static final By NEW_CARD_CVV = By.cssSelector("[data-testid='new-card-cvv']");
     public static final By SECURELY_PAY_BUTTON = By.xpath("//button[contains(normalize-space(),'Securely Pay')]");
+    
+    public void loginWithMobile(String mobileNumber) {
+        click(IxigoFlightBookingPage.LOGIN_SIGNUP_BUTTON);
+        type(IxigoFlightBookingPage.MOBILE_NUMBER_INPUT, mobileNumber);
+        click(IxigoFlightBookingPage.CONTINUE_BUTTON);
+        sleep(LOGIN_SETTLE_WAIT_MS);
+        sleep(LOGIN_SETTLE_WAIT_MS);
+    }
+
+    public void searchOneWayFlight(String fromCity, String toCity, By departureDateOption) {
+        clickNthSafe(IxigoFlightBookingPage.IXIGO_FLIGHTS_LINK, 1);
+        click(IxigoFlightBookingPage.ONE_WAY_TAB);
+
+        click(IxigoFlightBookingPage.FROM_SECTION);
+        type(IxigoFlightBookingPage.FROM_TEXT_INPUT, fromCity);
+        click(IxigoFlightBookingPage.NEW_DELHI_OPTION);
+
+        type(IxigoFlightBookingPage.TO_TEXT_INPUT, toCity);
+        click(IxigoFlightBookingPage.KOLKATA_OPTION);
+
+        click(IxigoFlightBookingPage.DEPARTURE_DATE);
+        click(departureDateOption);
+
+        clickAll(
+                IxigoFlightBookingPage.PAX_DROPDOWN,
+                IxigoFlightBookingPage.PAX_COUNT_ONE,
+                IxigoFlightBookingPage.DONE_BTN,
+                IxigoFlightBookingPage.SEARCH_BUTTON
+        );
+    }
+
+    public void proceedToBooking() {
+        click(IxigoFlightBookingPage.BOOK_BUTTON);
+        scrollIntoViewAndClick(IxigoFlightBookingPage.NO_FREE_RADIO);
+    }
+
+    public void fillTravellerDetails(String firstName, String lastName) {
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(IxigoFlightBookingPage.TITLE_DROPDOWN));
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView(true); window.scrollBy(0, -120);",
+                element
+        );
+        click(IxigoFlightBookingPage.TITLE_DROPDOWN);
+        click(IxigoFlightBookingPage.MR_TITLE);
+
+        type(IxigoFlightBookingPage.FIRST_NAME_INPUT, firstName);
+        type(IxigoFlightBookingPage.LAST_NAME_INPUT, lastName);
+
+        click(IxigoFlightBookingPage.ADULT_COUNTRY_INPUT);
+        click(IxigoFlightBookingPage.INDIA_COUNTRY_CODE);
+    }
+
+    public void continueToPayment() throws InterruptedException {
+        clickAll(
+                IxigoFlightBookingPage.CONTINUE_BUTTON,
+                IxigoFlightBookingPage.CONFIRM_BUTTON,
+                IxigoFlightBookingPage.NO_THANKS_BUTTON
+        );
+        Thread.sleep(10000);
+        clickAll(
+                IxigoFlightBookingPage.ADD_ON_TILE,
+                IxigoFlightBookingPage.MEAL_SELECTION_BUTTON,
+                IxigoFlightBookingPage.CONTINUE_BUTTON,
+                IxigoFlightBookingPage.CONTINUE_BUTTON,
+                IxigoFlightBookingPage.CONTINUE_TO_PAY_BUTTON
+        );
+    }
+
+    public void addCardAndPay(String cardNumber, String expiryDate, String cvv) {
+        click(IxigoFlightBookingPage.CARD_PAYMENT_OPTION);
+        click(IxigoFlightBookingPage.ADD_NEW_CARD);
+
+        type(IxigoFlightBookingPage.CARD_NUMBER, cardNumber);
+        type(IxigoFlightBookingPage.CARD_EXP_DATE, expiryDate);
+        type(IxigoFlightBookingPage.NEW_CARD_CVV, cvv);
+
+        click(IxigoFlightBookingPage.SECURELY_PAY_BUTTON);
+    }
+
+    protected void click(By by) {
+        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
+    }
+
+    protected void type(By by, String value) {
+        WebElement el = wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        el.clear();
+        el.sendKeys(value);
+    }
+
+    private void clickAll(By... locators) {
+        for (By locator : locators) {
+            click(locator);
+        }
+    }
+
+    private void scrollIntoViewAndClick(By by) {
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(by));
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView(true); window.scrollBy(0, -120);",
+                element
+        );
+        element.click();
+    }
+
+    private void clickNth(By by, int index) {
+        wait.until(driver -> {
+            List<WebElement> elements = driver.findElements(by);
+            return elements.size() > index ? elements : null;
+        });
+        List<WebElement> elements = driver.findElements(by);
+        wait.until(ExpectedConditions.elementToBeClickable(elements.get(index))).click();
+    }
+
+    private void clickNthSafe(By by, int index) {
+        try {
+            clickNth(by, index);
+        } catch (ElementClickInterceptedException ignored) {
+            // If a transient overlay intercepts this optional navigation click, proceed with next step.
+        }
+    }
+
+
+    private void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+    
 }
 
--- src/test/java/com/automation/tests/IxigoFlightBookingTest.java
+++ src/test/java/com/automation/tests/IxigoFlightBookingTest.java
@@ -1,187 +1,60 @@
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+package com.automation.tests;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import com.automation.base.BaseTest;
+import com.automation.config.ConfigReader;
 import com.automation.pages.IxigoFlightBookingPage;
+import com.automation.utils.ExcelUtil;
+import com.automation.utils.LoggerUtil;
 
-import java.time.Duration;
-import java.util.List;
+import java.util.Map;
 
-public class IxigoFlightBookingTest {
+public class IxigoFlightBookingTest extends BaseTest {
 
-    private static final String IXIGO_FLIGHTS_URL = "https://www.ixigo.com/flights";
-    private static final String MOBILE_NUMBER = "9667525275";
-    private static final String FROM_CITY = "New Delhi";
-    private static final String TO_CITY = "Kolkata";
-    private static final String FIRST_NAME = "Test";
-    private static final String LAST_NAME = "Account";
-    private static final String CARD_NUMBER = "4111 1111 1111 1111";
-    private static final String CARD_EXPIRY = "11/28";
-    private static final String CARD_CVV = "123";
-    private static final long LOGIN_SETTLE_WAIT_MS = 10000;
+    // --------------------------------------------------------
+    // Test 1: Search flights on Ixigo and complete till payment with credit card
+    // --------------------------------------------------------
 
-    private WebDriver driver;
-    private WebDriverWait wait;
+    private final LoggerUtil logger = LoggerUtil.getLogger(IxigoFlightBookingTest.class);
 
-    @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(25));
+    @SuppressWarnings("unchecked")
+    @Test(
+    		dataProvider = "flightBookingData",
+    		description = "Search flights on Ixigo and complete till payment with credit card"
+    	)
+
+        public void testIxigoFlow(Map<String, String> testData) throws InterruptedException {
+    	
+        String MOBILE_NUMBER = testData.get("Mobile");
+        String FROM_CITY =  testData.get("FromCity");
+        String TO_CITY =  testData.get("ToCity");
+        String FIRST_NAME =  testData.get("firstName");
+        String LAST_NAME =  testData.get("lastName");
+        String CARD_NUMBER =  testData.get("cardNumber");
+        String CARD_EXPIRY =  testData.get("exp");
+        String CARD_CVV =  testData.get("cvv");
+        
+    	log.info("Starting: IxigoFlightBookingTest");
+    	IxigoFlightBookingPage flightPage = new IxigoFlightBookingPage();
+    	log.info("=== Entering Mobioe number ====");
+    	flightPage.loginWithMobile(MOBILE_NUMBER);
+    	log.info("=== Enter Details ===");
+    	flightPage.searchOneWayFlight(FROM_CITY, TO_CITY, IxigoFlightBookingPage.JULY_10_2026_BUTTON);
+    	flightPage.proceedToBooking();
+    	log.info("== Enter passenger details ===");
+    	flightPage.fillTravellerDetails(FIRST_NAME, LAST_NAME);
+    	flightPage.continueToPayment();
+    	log.info("=== Enter credit card details === ");
+    	flightPage.addCardAndPay(CARD_NUMBER, CARD_EXPIRY, CARD_CVV);
+    	logger.info("=== Live ixigo Flight Booking Test PASSED ==="); 
     }
-
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
-    @Test
-    public void testIxigoFlow() throws InterruptedException {
-        openIxigoFlightsPage();
-        loginWithMobile(MOBILE_NUMBER);
-        searchOneWayFlight(FROM_CITY, TO_CITY, IxigoFlightBookingPage.JULY_10_2026_BUTTON);
-        proceedToBooking();
-        fillTravellerDetails(FIRST_NAME, LAST_NAME);
-        continueToPayment();
-        addCardAndPay(CARD_NUMBER, CARD_EXPIRY, CARD_CVV);
-    }
-
-    private void openIxigoFlightsPage() {
-        driver.get(IXIGO_FLIGHTS_URL);
-    }
-
-    private void loginWithMobile(String mobileNumber) {
-        click(IxigoFlightBookingPage.LOGIN_SIGNUP_BUTTON);
-        type(IxigoFlightBookingPage.MOBILE_NUMBER_INPUT, mobileNumber);
-        click(IxigoFlightBookingPage.CONTINUE_BUTTON);
-        sleep(LOGIN_SETTLE_WAIT_MS);
-        sleep(LOGIN_SETTLE_WAIT_MS);
-    }
-
-    private void searchOneWayFlight(String fromCity, String toCity, By departureDateOption) {
-        clickNthSafe(IxigoFlightBookingPage.IXIGO_FLIGHTS_LINK, 1);
-        click(IxigoFlightBookingPage.ONE_WAY_TAB);
-
-        click(IxigoFlightBookingPage.FROM_SECTION);
-        type(IxigoFlightBookingPage.FROM_TEXT_INPUT, fromCity);
-        click(IxigoFlightBookingPage.NEW_DELHI_OPTION);
-
-        type(IxigoFlightBookingPage.TO_TEXT_INPUT, toCity);
-        click(IxigoFlightBookingPage.KOLKATA_OPTION);
-
-        click(IxigoFlightBookingPage.DEPARTURE_DATE);
-        click(departureDateOption);
-
-        clickAll(
-                IxigoFlightBookingPage.PAX_DROPDOWN,
-                IxigoFlightBookingPage.PAX_COUNT_ONE,
-                IxigoFlightBookingPage.DONE_BTN,
-                IxigoFlightBookingPage.SEARCH_BUTTON
+    
+    @DataProvider(name = "flightBookingData")
+    public Object[][] flightBookingData() {
+        return ExcelUtil.getDataAsArray(
+                ConfigReader.getTestDataPath(),
+                "FlightBookingData"
         );
     }
-
-    private void proceedToBooking() {
-        click(IxigoFlightBookingPage.BOOK_BUTTON);
-        scrollIntoViewAndClick(IxigoFlightBookingPage.NO_FREE_RADIO);
     }
-
-    private void fillTravellerDetails(String firstName, String lastName) {
-        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(IxigoFlightBookingPage.TITLE_DROPDOWN));
-        ((JavascriptExecutor) driver).executeScript(
-                "arguments[0].scrollIntoView(true); window.scrollBy(0, -120);",
-                element
-        );
-        click(IxigoFlightBookingPage.TITLE_DROPDOWN);
-        click(IxigoFlightBookingPage.MR_TITLE);
-
-        type(IxigoFlightBookingPage.FIRST_NAME_INPUT, firstName);
-        type(IxigoFlightBookingPage.LAST_NAME_INPUT, lastName);
-
-        click(IxigoFlightBookingPage.ADULT_COUNTRY_INPUT);
-        click(IxigoFlightBookingPage.INDIA_COUNTRY_CODE);
-    }
-
-    private void continueToPayment() throws InterruptedException {
-        clickAll(
-                IxigoFlightBookingPage.CONTINUE_BUTTON,
-                IxigoFlightBookingPage.CONFIRM_BUTTON,
-                IxigoFlightBookingPage.NO_THANKS_BUTTON
-        );
-        Thread.sleep(10000);
-        clickAll(
-                IxigoFlightBookingPage.ADD_ON_TILE,
-                IxigoFlightBookingPage.MEAL_SELECTION_BUTTON,
-                IxigoFlightBookingPage.CONTINUE_BUTTON,
-                IxigoFlightBookingPage.CONTINUE_BUTTON,
-                IxigoFlightBookingPage.CONTINUE_TO_PAY_BUTTON
-        );
-    }
-
-    private void addCardAndPay(String cardNumber, String expiryDate, String cvv) {
-        click(IxigoFlightBookingPage.CARD_PAYMENT_OPTION);
-        click(IxigoFlightBookingPage.ADD_NEW_CARD);
-
-        type(IxigoFlightBookingPage.CARD_NUMBER, cardNumber);
-        type(IxigoFlightBookingPage.CARD_EXP_DATE, expiryDate);
-        type(IxigoFlightBookingPage.NEW_CARD_CVV, cvv);
-
-        click(IxigoFlightBookingPage.SECURELY_PAY_BUTTON);
-    }
-
-    private void click(By by) {
-        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
-    }
-
-    private void type(By by, String value) {
-        WebElement el = wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-        el.clear();
-        el.sendKeys(value);
-    }
-
-    private void clickAll(By... locators) {
-        for (By locator : locators) {
-            click(locator);
-        }
-    }
-
-    private void scrollIntoViewAndClick(By by) {
-        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(by));
-        ((JavascriptExecutor) driver).executeScript(
-                "arguments[0].scrollIntoView(true); window.scrollBy(0, -120);",
-                element
-        );
-        element.click();
-    }
-
-    private void clickNth(By by, int index) {
-        wait.until(driver -> {
-            List<WebElement> elements = driver.findElements(by);
-            return elements.size() > index ? elements : null;
-        });
-        List<WebElement> elements = driver.findElements(by);
-        wait.until(ExpectedConditions.elementToBeClickable(elements.get(index))).click();
-    }
-
-    private void clickNthSafe(By by, int index) {
-        try {
-            clickNth(by, index);
-        } catch (ElementClickInterceptedException ignored) {
-            // If a transient overlay intercepts this optional navigation click, proceed with next step.
-        }
-    }
-
-
-    private void sleep(long millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new RuntimeException(e);
-        }
-    }
-}
