1e595f98df5a1175cc4d9c4768b012f3b1dc329a
Shivam Rathour <66547505+shivam855@users.noreply.github.com>
2026-06-08 13:01:42 +0530

    Update IxigoFlightBookingTest.java

--- src/test/java/com/automation/tests/IxigoFlightBookingTest.java
+++ src/test/java/com/automation/tests/IxigoFlightBookingTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import com.qa.pages.IxigoFlightBookingPage;
+import com.automation.pages.IxigoFlightBookingPage;
 
 import java.time.Duration;
 import java.util.List;
